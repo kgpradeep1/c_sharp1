9b47006c5702f9bf07ba2559d7bfd1dfe212ac0e
kgpradeep1 <kgpra111@outlook.com>
2022-04-22 12:02:20 -0800

    commit adstraction,interface and inheritance

---
+++ Interface.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Concepts
+{
+    /// <summary>
+    /// INTERFACE-keyword interface-group of abstract methods and properties,no variables
+    /// INTERFACE-SECURITY
+    /// INTERFACE-MULTIPLE INTERFACES
+    /// </summary>
+    public interface IInterfaceOne
+    {
+        void Display();
+    }
+    public interface IInterfaceTwo
+    {
+        void Display();
+    }
+    /// <summary>
+    /// Sample is implementing IInterfaceOne, IInterfaceTwo
+    /// </summary>
+    public class Sample : IInterfaceOne, IInterfaceTwo
+    {
+        void IInterfaceOne.Display()
+        {
+            Console.WriteLine("Interface One");
+        }
+        void IInterfaceTwo.Display()
+        {
+            Console.WriteLine("Interface Two");
+        }
+    }
+
+    internal class Interface
+    {
+        /// <summary>
+        /// Base to Derived
+        /// </summary>
+        public static void Main()
+        {
+            IInterfaceOne interfaceone = new Sample();
+            interfaceone.Display();
+            IInterfaceTwo interfaceTwo = new Sample();
+            interfaceTwo.Display();
+        }
+
+
+    }
+}
---
+++ abstraction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Abstract1
+{
+    public abstract class Plan
+    {
+        protected double rate;
+        public abstract void getRate();
+        public void calculation(int units)
+        {
+            Console.Write("BILL AMOUNT FOR " + units + " UNITS is: Rs.");
+            Console.WriteLine(rate * units);
+        }
+    }
+
+    class CommercialPlan : Plan
+    {
+        public override void getRate()
+        {
+            rate = 5.00;
+        }
+    }
+    class DomesticlPlan : Plan
+    {
+        public override void getRate()
+        {
+            rate = 2.50;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Plan p;
+            Console.WriteLine("COMMERCIAL CONNECTION");
+            p = new CommercialPlan();
+            p.getRate();
+            p.calculation(250);
+            Console.WriteLine("DOMESTIC CONNECTION");
+            p = new DomesticlPlan();
+            p.getRate();
+            p.calculation(150);
+            
+        }
+    }
+}
