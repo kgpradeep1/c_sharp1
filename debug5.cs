dd555af5465e95089c3e3d9a0c7a97ad3328c382
kgpradeep1 <kgpra111@outlook.com>
2022-04-23 12:16:18 -0800

    commit generic

---
+++ debug5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace ErrorHandlingApplication
+{
+    class DivNumbers
+    {
+        int result;
+        DivNumbers()
+        {
+            result = 0;
+        }
+        public void division(int num1, int num2)
+        {
+            try
+            {
+                result = num1 / num2;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Exception caught: ", e);
+            }
+            finally
+            {
+                Console.WriteLine("Result: {0}", result);
+            }
+        }
+        static void Main()
+        {
+            DivNumbers d = new DivNumbers();
+            d.division(30,12);
+            Console.ReadKey();
+        }
+    }
+}
