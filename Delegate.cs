59f051d278b784baf6b5ff846b98e49debf51259
kgpradeep1 <kgpra111@outlook.com>
2022-04-23 17:35:56 -0800

    commit

---
+++ Delegate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace MulticastDelegateDemo
+{
+    public delegate void RectangleDelete(double Width, double Height);
+    public class Rectangle
+    {
+        public void GetArea(double Width, double Height)
+        {
+            Console.WriteLine(@"Area is {0}", (Width * Height));
+        }
+        public void GetPerimeter(double Width, double Height)
+        {
+            Console.WriteLine(@"Perimeter is {0}", (2 * (Width + Height)));
+        }
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            RectangleDelete rectDelegate = new RectangleDelete(rect.GetArea);
+            //RectangleDelete rectDelegate = rect.GetArea;
+
+            //binding a method with delegate object
+            // In this example rectDelegate is a multicast delegate. You use += operator 
+            // to chain delegates together and -= operator to remove.
+
+            rectDelegate += rect.GetPerimeter;
+            rectDelegate(23.45, 67.89);
+
+            Console.WriteLine();
+            rectDelegate.Invoke(13.45, 76.89);
+
+            Console.WriteLine();
+            //Removing a method from delegate object
+            rectDelegate -= rect.GetPerimeter;
+            rectDelegate.Invoke(13.45, 76.89);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExceptionHandling.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Concepts
+{
+    /// <summary>
+    /// STEP 1-Create a user-defined exception that inherits the base class EXCEPTION
+    /// </summary>
+    public class TrackerException : Exception
+    {
+        public TrackerException(string? message) : base(message)
+        {
+
+        }
+    }
+    internal class ExceptionsCustom
+    {
+        public static void Main()
+        {
+            int age_count;
+            try
+            {
+                
+                Console.WriteLine("Enter the age");//14
+                age_count = Convert.ToInt32(Console.ReadLine());
+                if (age_count < 18 && age_count > 18)
+                {
+                    throw (new TrackerException("not eligible"));
+                }
+                else
+                {
+                    Console.WriteLine("eligible");
+                }
+
+            }
+            catch (TrackerException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
